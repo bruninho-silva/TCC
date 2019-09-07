f35439d584c0799ee8aeb2be72839de11e1871f6
Bruno <bruno.depaula2006@hotmail.com>
2019-09-07 01:55:21 -0300

    inciado criação do core da lib

--- library/ConsoleApp1/ConsoleApp1/Cartas.cs
+++ library/ConsoleApp1/ConsoleApp1/Cartas.cs
@@ -1,11 +1,21 @@
 public class Cartas{
 
-    private Integer atributo1;
-  
-    public Integer getAtributo1{
-        return this.atributo1;
-    }
-    public void setAtributo1(Integer Atributo1){
-        this.atributo1 = atributo1;
-    }
+    private int miAtributo1;
+    private int lambAtributo1;
+    private int miAtributo2;
+    private int lambAtributo2;
+    private int miAtributo3;
+    private int lambAtributo3;
+    private int miAtributo4;
+    private int lambAtributo4;
+
+    public int miAtributo1 { get; set; }
+    public int miAtributo2 { get; set; }
+    public int miAtributo3 { get; set; }
+    public int miAtributo4 { get; set; }
+    public int lambAtributo1 { get; set; }
+    public int lambAtributo2 { get; set; }
+    public int lambAtributo3 { get; set; }
+    public int lambAtributo4 { get; set; }
+
 }
--- library/ConsoleApp1/ConsoleApp1/LogicaParaconsistente.cs
+++ library/ConsoleApp1/ConsoleApp1/LogicaParaconsistente.cs
@@ -1,86 +1,125 @@
-public class LogicaParaconsistente implements{
-    
-public Integer obtemPorcentagemDeDano(Cartas[] cartas){
-    validaGameObject(cartas);
-    Baricentro baricentro = obtemBaricentro(cartas)
-    Double gC = calculaGrauDeCerteza(baricentro);
-    Double gI = calculaGrauDeIncerteza(baricentro);
-    String estadoLogico = descobreEstadoLogico(gC,gI);
-    return transformaEstadoLogicoEmPorcentagem(estadoLogico);
-}
+using System;
+using System.Collections.Generic;
 
-public void descobreEstadoLogico(Double gc, Double gi){
-
-    if (gc > 0  && gi > 0)
-    {
-        if (gc == 0 )
-        {
-            return Constants.INCOSISTENTE;
-        }
-        if (gi == 0)
-        {
-            return Constants.VERDADE;
-        }
-        
-        if (gc > gi)
-        {
-           return Constants.INCONSISTENTE_TENTE_VERDADE ;
-        }
-        if (gi > gc){
-           return Constants.VERDADE_TENDE_INCONSISTENTE;
-        }
-        
-    }
-    if (gc < 0 && gi < 0)
-    {
-        
-    }
-    if (gc > 0 && gi < 0)
-    {
-        
-    }
-    if (gc < 0 && gi > 0)
-    {
-        
-    }
-
-if (gc == 0)
+public class LogicaParaconsistente
 {
-    if (gi > 0)
-    {
-      Inconsistemte  
+    readonly double vcve = 0.5; //Var de controle de Veracidade
+    readonly double vcfa = 0.5; //Var de controle de Falsidade
+    readonly double vcic = 0.5; //var de conttole de Inconsistencia
+    readonly double vcpa = 0.5; //var de controle de Paracompleto
+
+    public int obtemPorcentagemDeDano(Cartas[] cartas){
+        if (validaGameObject(cartas))
+        {
+            Baricentro baricentro = obtemBaricentro(cartas);
+            Double gC = calculaGrauDeCerteza(baricentro);
+            Double gI = calculaGrauDeIncerteza(baricentro);
+            String estadoLogico = descobreEstadoLogico(gC, gI);
+            return transformaEstadoLogicoEmPorcentagem(estadoLogico);
+        }
+        else
+        {
+            return 0;
+        }
     }
-     if (gi < 0)
-    {
-       Paraconpleto 
+
+    public void descobreEstadoLogico(Double gc, Double gi){
+
+
+        if (gc > vcve)
+        {
+            // Verdade 
+        }
+        else if (gc < vcfa)
+        {
+            // Falso 
+        }
+        else if (gi > vcic)
+        {
+            // Inconsistencia
+        }
+        else if (gi > vcpa)
+        {
+            // Paracompleto
+        }
+        else if (gc >= 0 && gc < vcve && gi >= 0 && gi < vcic && gc >= gi)
+        {
+            // Quase Verdade tedendo a inconsistente
+        }
+        else if (gc >= 0 && gc < vcve && gi >= 0 && gi < vcic && gc < gi)
+        {
+            // Incosistente tedendo a verdade
+        }
+        else if (gc >= 0 && gc < vcve && gi > vcpa && gi <= 0 && gc >= Math.Abs(gi))
+        {
+            // Quase Verdadeiro tendendo a Paracompleto
+        }
+        else if (gc >= 0 && gc < vcve && gi > vcpa && gi <= 0 && gc >= Math.Abs(gi))
+        {
+            // Paracompleto tendendo a Verdadeiro
+        }
+        else if (gc > vcfa && gc <= 0 && gi > vcpa && gi <= 0 && Math.Abs(gc) > Math.Abs(gi))
+        {
+            // Falso tendendo paracompleto
+        }
+        else if (gc > vcfa && gc <= 0 && gi > vcpa && gc < gi && gi <= 0)
+        {
+            // Paracompleto tendendo a falso
+        }
+        else if (gc > vcfa && gc <= 0 && gi >= 0 && gi < vcic && gc >= gi)
+        {
+            // Falso tedendo a inconsistente
+        }
+        else if (gc <= 0 &&  gc > vcfa && gi >= 0 && gi < vcic && gc < gi)
+        {
+            // Inconsistente tendendo a falso
+        }
     }
-}
 
 
+    public void transformaEstadoLogicoEmPorcentagem(String estadoLogico){}
 
+    public Baricentro obtemBaricentro(Cartas[] objetos){
+        int posicao = 0;
+        int[] arrayMi1 = new int[] { objetos[0].miAtributo1, objetos[1].miAtributo1, objetos[2].miAtributo1,
+                                  objetos[3].miAtributo1, objetos[4].miAtributo1, objetos[5].miAtributo1,
+                                  objetos[6].miAtributo1, objetos[7].miAtributo1 ,objetos[8].miAtributo1 };
+        int[] listaLamb1 = new int[] { objetos[0].lambAtributo1, objetos[1].lambAtributo1, objetos[2].lambAtributo1,
+                                  objetos[3].lambAtributo1, objetos[4].lambAtributo1, objetos[5].lambAtributo1,
+                                  objetos[6].lambAtributo1, objetos[7].lambAtributo1 ,objetos[8].lambAtributo1 };
+            
+        return new Baricentro();
+    }
 
-}
+    public Boolean validaGameObject(Cartas[] objetos) {
+        if (objetos.Length == 8)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
 
-public void transformaEstadoLogicoEmPorcentagem(){}
+    public Double ConverteNumeros(int numeroInteiro)
+    {
+        return Convert.ToDouble(numeroInteiro)
+    }
 
-public Baricentro obtemBaricentro(Cartas[] objetos){}
+    public Double CalculaGrauDeCerteza(Baricentro baricentro)
+    {
+        Double mi = baricentro.mi;
+        Double lambida = baricentro.lambida;
+        return mi + lambida - 1;
+    }
 
-public void validaGameObject(cartas[] objetos) trows GameObjectInvalido {}
-
-public Double converteNumeros(Integer numeroInteiro){}
-
-public Double calculaGrauDeCerteza(Baricentro baricentro){
-    Double mi = baricentro.getMi();
-    Double lambida = baricentro.getLambida();
-    return mi + lambida - 1
-}
-
-public Double calculaGrauDeIncerteza(Baricentro baricentro){
-    Double mi = baricentro.getMi();
-    Double lambida = baricentro.getLambida();
-    return lambida - mi;
-}
+    public Double CalculaGrauDeIncerteza(Baricentro baricentro)
+    {
+        Double mi = baricentro.mi;
+        Double lambida = baricentro.lambida;
+        return lambida - mi;
+    }
     
-public void descobreEstadoLogico(Double grauDeCerteza, Double grauDeIncerteza){}
-
+   
 }
