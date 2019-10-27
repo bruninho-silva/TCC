8d37b6cdcef64707fb977c22bf2b18507ebb5230
Gustavo Marques <gustavo.fs.marques@gmail.com>
2019-10-26 22:12:37 -0300

    Normalização das variáveis mi e lambda.

--- library/ConsoleApp1/ConsoleApp1/Baricentro.cs
+++ library/ConsoleApp1/ConsoleApp1/Baricentro.cs
@@ -8,5 +8,14 @@ public class Baricentro{
 
     public int Lambda { get; set; }
 
-
+    public double MiNormalizado()
+    {
+        double value = Convert.ToDouble(Mi) / 100;
+        return value;
+    }
+    public double LambdaNormalizado()
+    {
+        double value = Convert.ToDouble(Lambda) / 100;
+        return value;
+    }
 }
