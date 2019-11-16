1e065f756cfe7131823dd5cd103e4c09d77fddad
Gustavo Marques <gustavo.fs.marques@gmail.com>
2019-11-16 02:37:00 -0300

    Refatoração do código da biblioteca.

---
+++ library/ConsoleApp1/ConsoleApp1/EstadoLogico.cs
@@ -0,0 +1,104 @@
+﻿using System;
+
+public static class EstadoLogico
+{
+    public static string DescobreEstadoLogico(double gc, double gi)
+    {
+
+        if (gc >= Constante.VCVE)
+        {
+            return Constante.VERDADE;
+        }
+        else if (gc <= Constante.VCFA)
+        {
+            return Constante.FALSO;
+        }
+        else if (gi >= Constante.VCIC)
+        {
+            return Constante.INCONSISTENTE;
+        }
+        else if (gi <= Constante.VCPA)
+        {
+            return Constante.PARACOMPLETO;
+        }
+
+        else if ((gc >= 0 && gc < Constante.VCVE) && (gi >= 0 && gi < Constante.VCIC))
+        {
+            if (gc >= gi)
+            {
+                return Constante.QUASE_V_I;
+            }
+            else
+            {
+                return Constante.INCONSISTENTE_T_VERDADE;
+            }
+        }
+        else if ((gc >= 0 && gc < Constante.VCVE) && (gi > Constante.VCPA && gi <= 0))
+        {
+            if (gc >= Math.Abs(gi))
+            {
+                return Constante.QUASE_V_P;
+            }
+            else
+            {
+                return Constante.PARACOMPLETO_T_VERDADE;
+            }
+        }
+        else if ((gc > Constante.VCFA && gc <= 0) && (gi > Constante.VCPA && gi <= 0))
+        {
+            if (Math.Abs(gc) > Math.Abs(gi))
+            {
+                return Constante.QUASE_F_P;
+            }
+            else
+            {
+                return Constante.PARACOMPLETO_T_FALSO;
+            }
+        }
+        else if ((gc > Constante.VCFA && gc <= 0) && (gi >= 0 && gi < Constante.VCIC))
+        {
+            if (Math.Abs(gc) >= gi)
+            {
+                return Constante.QUASE_F_I;
+            }
+            else
+            {
+                return Constante.INCONSISTENTE_T_FALSO;
+            }
+        }
+        return null;
+    }
+
+    public static int TransformaEstadoLogicoEmPorcentagem(string estadoLogico)
+    {
+        switch (estadoLogico)
+        {
+            case Constante.VERDADE:
+                return 100;
+            case Constante.FALSO:
+                return 0;
+            case Constante.INCONSISTENTE:
+                return 12;
+            case Constante.PARACOMPLETO:
+                return 10;
+            case Constante.QUASE_V_I:
+                return 35;
+            case Constante.INCONSISTENTE_T_VERDADE:
+                return 25;
+            case Constante.QUASE_V_P:
+                return 60;
+            case Constante.PARACOMPLETO_T_VERDADE:
+                return 49;
+            case Constante.QUASE_F_P:
+                return 89;
+            case Constante.PARACOMPLETO_T_FALSO:
+                return 45;
+            case Constante.QUASE_F_I:
+                return 8;
+            case Constante.INCONSISTENTE_T_FALSO:
+                return 85;
+
+        }
+        return 0;
+    }
+}
