ef95af919d246584e6c9a4b54188740b3decd8aa
DanielSDOliveira <danieldavid0302@gmail.com>
2019-12-08 05:37:53 -0300

    Game Completo

--- game/FightingAgainstMonsters/Assets/Fighting Against Monsters/Scripts/UIMenu/Hyperlink.cs
+++ game/FightingAgainstMonsters/Assets/Fighting Against Monsters/Scripts/UIMenu/Hyperlink.cs
@@ -4,26 +4,22 @@ using UnityEngine;
 
 public class Hyperlink : MonoBehaviour
 {
-    private readonly string urlunip = "https://www.unip.br";
-    private readonly string urldevdoo = "http://www.devdoo.com";
-    private readonly string urlmoodle = "http://blog.unicluster.com.br/";
-    public string urloption;
-    public void OpenLink()
-    {
-        var url = "";
 
-        if (urloption == "unip")
-        {
-            url = urlunip;
-        }
-        else if (urloption == "devdoo")
-        {
-            url = urldevdoo;
-        }
-        else if (urloption == "moodle")
-        {
-            url = urlmoodle;
-        }
-        Application.OpenURL(url);
+    public void moodleurl()
+    {
+        Application.OpenURL("http://blog.unicluster.com.br/");
+
     }
+
+    public void devdoourl()
+    {
+        Application.OpenURL("http://www.devdoo.com");
+    }
+
+    public void uniprurl()
+    {
+        Application.OpenURL("https://www.unip.br/");
+    }
+
+   
 }
